568c09d9832979190acebe0464b2e46fdaedf0f9
Daria <darina.vaxrusheva@bk.ru>
2022-10-04 15:14:14 +0400

    h

---
+++ fanction.py
@@ -0,0 +1,10 @@
+
+
+def new_contact():
+    name = input("введите ФИО контакта: ")
+    tel = input("введите немер телефона контакта: ")
+    print(f'Ваши данные {name}, {tel} успешно сохранены')
+    return name, tel
+
+
+    
---
+++ main.py
@@ -0,0 +1,13 @@
+from fanction import new_contact
+from decision import read_book
+
+book = input('Что вам требуется: \n0 - новый контакт\n1 - просмотр книги \n')
+
+if book == "0":
+    new_contact()
+elif book == "1":
+    read_book()
+else:
+    book != 1 and book != 0
+    print("Введите корректное значение! ")    
+    book = input('Что вам требуется: \n0 - новый контакт\n1 - просмотр книги \n')

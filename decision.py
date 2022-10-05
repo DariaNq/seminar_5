3b2ba2f4b928f4b7a0ca29790018068f1300bc67
Daria <darina.vaxrusheva@bk.ru>
2022-10-05 13:50:57 +0400

    I

--- decision.py
+++ decision.py
@@ -1,13 +1,24 @@
-from fanction import new_contact
+from fanction import name_data, phone_data, surname_data
 
 
-def new_name():
-    with open('phone_book.txt', 'a+') as file:
-       file.write(f'{new_contact} \n')
+def new_contact():
+    name = name_data()
+    phone = phone_data()
+    surname = surname_data()
+    with open('data_first_variant.csv', 'a', encoding='utf-8') as file:
+            file.write(f'{name}: {surname}: {phone}\n')
 
 
 def read_book():
-    print("Ваша телефонная книга: \n")
-    with open('data_first_variant.csv', 'a', encoding='utf-8') as file:
-        file.write(f'{phone_book.txt}\n')
-
+    print('Ваша телефонная книга: \n')
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as file:
+        data_first = file.readlines()
+        data_first_version_second = []
+        data_middle = ''
+        j = 0
+        for i in range(len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_version_second.append(''.join(data_first[j:i + 1]))
+                j = i
+        data_first = data_first_version_second
+        print(''.join(data_first))
